5ec2c1b55d74aebb5ee80c97e74814573aed53a5
islam <cloplop091@gmail.com>
2023-10-23 05:52:55 +0300

    urls update

--- service/library_api/urls.py
+++ service/library_api/urls.py
@@ -5,5 +5,5 @@ from .views import ListBookAPI, CreateBookAPI, UpdateDestroyBookAPI
 urlpatterns = [
     path('books/', ListBookAPI.as_view(), name='books'),
     path('books/create/', CreateBookAPI.as_view(), name='create_book'),
-    path('books/<int:pk>/', UpdateDestroyBookAPI.as_view(), name='update_destroy_book'),
+    path('api/books/<uuid:pk>/', UpdateDestroyBookAPI.as_view(), name='update_destroy_book'),
 ]
